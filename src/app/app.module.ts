7ae31fc69e08ef7cefe5aa8f5752919ccb89f5e2
mccarthyr <mccarthy.richard@gmail.com>
2018-01-16 14:41:09 +0000

    Added new messages component, app.modules.ts has been udpated to reflect this

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent }        from './app.component';
 import { HeroesComponent }     from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService }         from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,

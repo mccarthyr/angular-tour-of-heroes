5e574a12a7b2c5365fec694ad2748d890efe4891
mccarthyr <mccarthy.richard@gmail.com>
2018-01-16 18:10:47 +0000

    Removed @NgModule.declarations and @NgModule.imports and added RouterModule and Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [ RouterModule ]
 })
+
 export class AppRoutingModule { }
+
+
+
+/*
+ You generall don't declare components in a routing module so the
+ originally generated CommonModule that was imported and the
+ @NgModule.declarations array have been removed.
+
+ Exporting RouterModule makes router directives available for
+ use in the AppModule components that will need them.
+ */
+ 

33b9d4df4f1c38eee5a410a189e53dccf13b9ff1
mccarthyr <mccarthy.richard@gmail.com>
2018-01-16 18:05:12 +0000

    Generated AppRoutingModule and updated the app.module.ts to import it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,9 @@ import { AppComponent }        from './app.component';
 import { HeroesComponent }     from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService }         from './hero.service';
-import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './message.service';
+import { MessagesComponent }   from './messages/messages.component';
+import { MessageService }      from './message.service';
+import { AppRoutingModule }    from './/app-routing.module';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { MessageService } from './message.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [ HeroService, MessageService  ],
   bootstrap: [AppComponent]

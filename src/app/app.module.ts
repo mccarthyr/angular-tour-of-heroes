d8f4036ab6eed39d0177cb83b33f31dae8685154
mccarthyr <mccarthy.richard@gmail.com>
2018-01-19 11:35:35 +0000

    Generated new hero-search component, app.module delcarations updated and selector added to dashboard template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MessagesComponent }               from './messages/messages.component';
 import { MessageService }                  from './message.service';
 import { AppRoutingModule }                from './/app-routing.module';
 import { DashboardComponent }              from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { DashboardComponent }              from './dashboard/dashboard.component
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
